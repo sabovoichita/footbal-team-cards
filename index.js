70d5cd422b339eb4e03d20108a118e3c1980e66a
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-18 16:18:50 +0100

    2. Next, access the id called "sport" from the HTML document and store it in a const variable called typeOfSport. Below that variable, assign the id of "year" to a const variable called worldCupYear.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const teamName = document.getElementById("team");
+const typeOfSport = document.getElementById("sport");
+const worldCupYear = document.getElementById("year");
 console.log(teamName);
+console.log(typeOfSport);
+console.log(worldCupYear);
