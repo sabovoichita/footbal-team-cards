b81a4d9ee3a87ebb2152ec61b0d9d353457e8afb
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-18 18:48:49 +0100

    43. Add a new case for "defender" that checks if player.position equals "defender" following the same pattern as the previous step.

--- index.js
+++ index.js
@@ -207,6 +207,11 @@ playersDropdownList.addEventListener("change", (e) => {
         players.filter((player) => player.position === "midfielder")
       );
       break;
+    case "defender":
+      setPlayerCards(
+        players.filter((player) => player.position === "defender")
+      );
+      break;
   }
 });
 
